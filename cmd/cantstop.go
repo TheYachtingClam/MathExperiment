5bda72b33b9bf77eda8c2da7131cf81927ed45e7
Nicholas Mayne <nick@modelrocket.io>
2023-03-10 11:57:46 -0800

    initial setup

---
+++ cmd/cantstop.go
@@ -0,0 +1,11 @@
+// My tools tt simulate Can't Stop the board game
+
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("Can't Stop")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TheYachtingClam/MathExperiment
+
+go 1.19
